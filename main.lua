ebd2327df7899c4fc785be93071644c2974611a1
Ashutosh Kumar <ashutosh.kumar@digantara.co.in>
2023-07-12 14:17:59 +0530

    feature: movement implement

--- main.lua
+++ main.lua
@@ -1,14 +1,26 @@
 debug = true
-palyerImg = nil
+player = { x = 200, y = 645, speed = 150, img = nil }
 
 function love.load(arg)
-    palyerImg = love.graphics.newImage("assets/img/plane.png")
+    player.img = love.graphics.newImage("assets/img/plane.png")
 end
 
 function love.update(dt)
+    if love.keyboard.isDown('escape') then
+        love.event.push('quit')
+    end
 
+    if love.keyboard.isDown('left', 'a') then
+        if player.x >= 0 then
+            player.x = player.x - player.speed*dt
+        end
+    elseif love.keyboard.isDown('right', 'd') then
+        if player.x <= (love.graphics.getWidth() - player.img:getWidth()) then
+            player.x = player.x + player.speed*dt
+        end
+    end
 end
 
 function love.draw(dt)
-    love.graphics.draw(palyerImg,100,100)
+    love.graphics.draw(player.img,player.x,player.y)
 end
