c76945e9c1a3e3886a4ccdb3c813049f802d5edd
Ashutosh Kumar <ashutosh.kumar@digantara.co.in>
2023-07-12 12:15:20 +0530

    feature: basic game app

---
+++ conf.lua
@@ -0,0 +1,3 @@
+function love.conf(t)
+    t.title = "Scrooling shooter example"
+end
--- main.lua
+++ main.lua
@@ -1,3 +1,22 @@
-function love.draw()
-    love.graphics.print("Hello world", 400, 300)
+-- function love.load()
+--    whale = love.graphics.newImage("fire.png")
+-- end
+-- function love.draw()
+--    love.graphics.draw(whale, 300, 200)
+-- end
+-- function love.load()
+--    sound = love.audio.newSource("fire.ogg", "stream")
+--    love.audio.play(sound)
+-- end
+playerImg = nil -- this is just for storage
+
+function love.load(arg)
+	playerImg = love.graphics.newImage('assets/img/plane.png')
+    --we now have an asset ready to be used inside Love
 end
+
+function love.draw(dt)
+   love.graphics.draw(playerImg, 100, 100)
+--    love.graphics.draw(player.img, player.x, player.y)
+end
+
